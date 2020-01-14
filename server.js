81393d50b723cf489fb2971655ced6a88ebf4184
Jessica Camila <jessica.camila@gmail.com>
2020-01-13 21:23:38 -0300

    comitando primeira versão do projeto.

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+//iniciando o app...
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//conectando o DB...
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi",
+    {useNewUrlParser: true}
+);
+requireDir("./src/models");
+
+
+//rota...
+app.use("/api", require("./src/routes"));
+
+
+app.listen(3001);
